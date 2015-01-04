9e853c7860f9623902f4ec3ba61ee50606f1e162
Deny Dias <deny@macpress.com.br>
2015-01-04 19:15:15 -0200

    Remove raise on focus.
    - Raise on focus is annoying and don't give the expected behavior. It just brings to front a client that is in the current tag, but has no effect on clients in another, not visible tag. The expected behavior is either:
      1. Switch to the tag where the client is in;
      2. Enable viewing of the tag where the client is in.

--- rc.lua
+++ rc.lua
@@ -664,7 +664,6 @@ client.connect_signal("focus",
             c.border_color = beautiful.border_focus
             -- Also raise the client when it get the focus
             client.focus = c
-            c:raise()
         end
     end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
